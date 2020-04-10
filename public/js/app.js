3443d6d2051298f343e5a204d1c4aafa36f3ef9c
ravelinodecastro <ravelinodecastro@andorinha.co.ao>
2020-04-10 14:43:22 +0100

    app.js in public updated

--- public/js/app.js
+++ public/js/app.js
@@ -35619,6 +35619,7 @@ window._ = __webpack_require__(/*! lodash */ "./node_modules/lodash/lodash.js");
 
 window.axios = __webpack_require__(/*! axios */ "./node_modules/axios/index.js");
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.baseURL = document.head.querySelector('meta[name="api-base-url"]').content;
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
