abd7d4857ac68a2eac42b9e27eb61edaa3b28800
Ravelino De Castro <43408517+ravelinodecastro@users.noreply.github.com>
2020-05-08 17:02:28 +0100

    redirect fixed

--- app/Http/Controllers/Admin/Auth/LoginController.php
+++ app/Http/Controllers/Admin/Auth/LoginController.php
@@ -27,7 +27,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/admin';
+    protected $redirectTo = '/admin/dashboard';
 
     /**
      * Create a new controller instance.
